abad905e8a72203c796ab324994c33e81dfc8270
Andrea Damiani <andreadamiani90@gmail.com>
2015-05-20 14:45:06 +0200

    Project Setup

---
+++ src/Agent.java
@@ -0,0 +1,19 @@
+public class Agent extends jade.core.Agent {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2139586581094493078L;
+	
+	@Override
+	protected void setup() {
+		// TODO Auto-generated method stub
+		super.setup();
+	}
+	
+	@Override
+	protected void takeDown() {
+		// TODO Auto-generated method stub
+		super.takeDown();
+	}
+}
